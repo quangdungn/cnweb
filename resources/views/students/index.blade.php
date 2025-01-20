0c697efdeee326375bbd53a82a3fbdabd3e998a5
quangdungn <quangdungnguyen.name@gmail.com>
2025-01-20 22:07:59 +0700

    updatePr

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,7 +7,7 @@
 			<div class="table-title">
 				<div class="row">
 					<div class="col-sm-6">
-						<h2>Manage <b>Employees</b></h2>
+						<h2>Students <b>Management</b></h2>
 					</div>
 					
 					<div class="col-sm-6">
